606087f0b8a9dc0255790a6e4c87df24e56227a0
Marcin Trofimiuk <marcin@infodiagram.com>
2024-12-02 11:33:08 +0100

    Openai provider

---
+++ providers/openai/src/lib/index.ts
@@ -0,0 +1,78 @@
+'use strict'
+
+import OpenAI from 'openai'
+
+interface ProviderOptions {
+  apiKey?: string
+  localeMap?: Record<string, string>
+  apiOptions?: Record<string, any>
+}
+
+interface TranslateOptions {
+  text: string | string[] | any[]
+  sourceLocale: string
+  targetLocale: string
+  priority: number
+  format?: 'plain' | 'markdown' | 'html'
+}
+
+
+const OpenAIProvider = {
+  provider: 'openai',
+  name: 'OpenAI',
+
+  init(providerOptions: ProviderOptions = {}) {
+    // Mocked initialization logic
+    console.log('Initializing OpenAI provider with options:', providerOptions)
+    const client = new OpenAI({
+      apiKey: providerOptions.apiKey,
+    })
+    return {
+      async translate({ text, sourceLocale, targetLocale, format }: TranslateOptions): Promise<string[]> {
+        const prompt = `Translate the following text from ${sourceLocale} to ${targetLocale}: \n${JSON.stringify(text, null, 2)}`
+        // Mocked translate function with static response
+        console.log('Translating text:', text)
+        console.log('Prompt: ', prompt)
+        console.log('Format:', format)
+
+        const response = await client.chat.completions.create({
+          model: 'gpt-4o-mini',
+          messages: [
+            {
+              role: 'system',
+              content: 'Do not add any additional information like that you are trained on data up to certain date',
+            },
+            {
+              role: 'system',
+              content: 'The text to translate is provided json format array. Respond also in json array format.'
+            },
+            {
+              role: 'system',
+              content: 'If source text is not provided return empty response.',
+            },
+            {
+              role: 'system',
+              content: 'Respond only with translated text. Without any additional information.',
+            },
+            { role: 'user', content: prompt },
+          ],
+        })
+        const translatedText = response.choices[0].message.content
+        console.log("Translated text: ", translatedText)
+        if (!translatedText) {
+          throw Error("Translation text empty!")
+        }
+        return JSON.parse(translatedText)
+      },
+      async usage() {
+        // Mocked usage function with static response
+        console.log('Fetching usage data')
+        return 1000 // Mocked usage data
+      },
+    }
+  },
+}
+
+export default OpenAIProvider
+// @ts-ignore
+module.exports = OpenAIProvider
